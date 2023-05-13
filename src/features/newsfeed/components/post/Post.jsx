04bcd80f35ecac51c3dea6d721af697c6f2d035f
Łukasz Drobnik <drobniklukasz@gmail.com>
2023-05-13 17:57:24 +0200

    Add mock icon buttons to Post

--- src/features/newsfeed/components/post/Post.jsx
+++ src/features/newsfeed/components/post/Post.jsx
@@ -1,12 +1,24 @@
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
 
 export default function Post(props) {
   return (
     <Card>
       <CardMedia component="img" image={props.url} alt={props.title} />
       <CardContent>{props.title}</CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
