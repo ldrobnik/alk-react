f036b70f7078058c0bdaf1a3612c397d50ad1b39
Łukasz Drobnik <drobniklukasz@gmail.com>
2023-05-13 18:17:53 +0200

    Adjust Post styles

--- src/features/newsfeed/components/post/Post.jsx
+++ src/features/newsfeed/components/post/Post.jsx
@@ -7,6 +7,8 @@ import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 
+import { PostWrapper } from "./Post.styles";
+
 export default function Post(props) {
   // Capitalizes the first letter of a string
   const capitalizeFirstLetter = (string) => {
@@ -18,19 +20,23 @@ export default function Post(props) {
   };
 
   return (
-    <Card>
-      <CardMedia component="img" image={props.url} alt={props.title} />
-      <CardContent>
-        <Typography>{capitalizeFirstLetter(props.title)}</Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
+    <PostWrapper>
+      <Card>
+        <CardMedia component="img" image={props.url} alt={props.title} />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {capitalizeFirstLetter(props.title)}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="share">
+            <ShareIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    </PostWrapper>
   );
 }
