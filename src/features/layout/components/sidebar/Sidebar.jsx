45d332439e08fc13d8c51f942c0f3faf69e0022d
Łukasz Drobnik <drobniklukasz@gmail.com>
2023-05-13 12:13:19 +0200

    Handle sidebar state based on current path

--- src/features/layout/components/sidebar/Sidebar.jsx
+++ src/features/layout/components/sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import HomeIcon from "@mui/icons-material/Home";
@@ -12,11 +13,38 @@ const buttonIcons = {
 };
 
 export default function Sidebar() {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState("");
+  const [currentPage, setCurrentPage] = useState("home");
+
+  useEffect(() => {
+    setCurrentPath(location.pathname);
+  }, [location]);
+
+  useEffect(() => {
+    // Identifies the sidebar button to be active based on the current path.
+    const currentButton = SIDEBAR_BUTTONS.find(
+      (button) => button.path === currentPath
+    );
+
+    // Sets the current page according to the index.
+    if (currentButton) {
+      setCurrentPage(currentButton.value);
+    }
+  }, [currentPath]);
+
   return (
-    <ToggleButtonGroup orientation="vertical" value="home" exclusive>
+    <ToggleButtonGroup orientation="vertical" value={currentPage} exclusive>
       {SIDEBAR_BUTTONS.map((button) => {
         return (
-          <ToggleButton key={button.value} value={button.value}>
+          <ToggleButton
+            key={button.value}
+            value={button.value}
+            sx={{
+              border: "none",
+              borderRadius: "0%",
+            }}
+          >
             <Link to={button.path}>{buttonIcons[button.value]}</Link>
           </ToggleButton>
         );
--- src/features/layout/components/sidebar/Sidebar.styles.js
+++ src/features/layout/components/sidebar/Sidebar.styles.js
@@ -1,6 +1,1 @@
 import { styled } from "@mui/material/styles";
-
-export const SidebarTile = styled("div")({
-  width: "100%",
-  textAlign: "center",
-});
