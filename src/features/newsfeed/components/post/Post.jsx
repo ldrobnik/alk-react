baa8bf8cad53d11cdfb04f0d876c5af37e60edfa
Łukasz Drobnik <drobniklukasz@gmail.com>
2023-05-13 18:03:32 +0200

    Adjust text content in Post

--- src/features/newsfeed/components/post/Post.jsx
+++ src/features/newsfeed/components/post/Post.jsx
@@ -2,15 +2,27 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 
 export default function Post(props) {
+  // Capitalizes the first letter of a string
+  const capitalizeFirstLetter = (string) => {
+    const firstLetter = string.charAt(0);
+    const firstLetterCap = firstLetter.toUpperCase();
+    const remainingLetters = string.slice(1);
+    const capitalizedText = firstLetterCap + remainingLetters;
+    return capitalizedText;
+  };
+
   return (
     <Card>
       <CardMedia component="img" image={props.url} alt={props.title} />
-      <CardContent>{props.title}</CardContent>
+      <CardContent>
+        <Typography>{capitalizeFirstLetter(props.title)}</Typography>
+      </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
